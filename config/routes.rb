2ad84a123f5762da2f5a65804eecc7d9ec7f360f
tomohiromiyamoto <69227948+tomohiromiyamoto@users.noreply.github.com>
2020-08-12 13:03:33 +0900

    Revert "チャットルーム管理機能の実装"

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users
-  root "rooms#index"
+  root "messages#index"
   resources :users, only: [:edit, :update]
-  resources :rooms, only: [:new, :create]
 end
